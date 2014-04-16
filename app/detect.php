7375b27a00512a12d161c8b8483deb7485db440c
sant0sh <san00718@gmail.com>
2014-04-17 00:38:19 +0530

    User agent checker modifie

--- app/detect.php
+++ app/detect.php
@@ -20,9 +20,9 @@ elseif(stripos($_SERVER['HTTP_USER_AGENT'],"Android"))
 $_REQUEST['Android'] = 1;
 
 }
-elseif(stripos($_SERVER['HTTP_USER_AGENT'],"IEMobile"))
+elseif(stripos($_SERVER['HTTP_USER_AGENT'],"IEweb"))
 {
-$_REQUEST['IEMobile'] = 1;
+$_REQUEST['IEweb'] = 1;
 
 }
 elseif(stristr($userBrowser, 'DoCoMo/' || 'portalmmm/'))
@@ -41,67 +41,67 @@ $_REQUEST['html'] = 1;
 
 
 if(!defined('WAP'))
-	define('WAP', isset($_REQUEST['wap']) || isset($_REQUEST['wap2']) || isset($_REQUEST['imode'])|| isset($_REQUEST['html'])|| isset($_REQUEST['Android'])|| isset($_REQUEST['iphone'])|| isset($_REQUEST['IEMobile']));
+	define('WAP', isset($_REQUEST['wap']) || isset($_REQUEST['wap2']) || isset($_REQUEST['imode'])|| isset($_REQUEST['html'])|| isset($_REQUEST['Android'])|| isset($_REQUEST['iphone'])|| isset($_REQUEST['IEweb']));
 	
 	if (WAP)
 {
-	define('WIRELESS_PROTOCOL', isset($_REQUEST['wap']) ? 'wap' : (isset($_REQUEST['wap2']) ? 'wap2' : (isset($_REQUEST['iphone']) ? 'iphone' : (isset($_REQUEST['imode']) ? 'imode' : (isset($_REQUEST['IEMobile']) ? 'IEMobile' :(isset($_REQUEST['html']) ? 'html' : (isset($_REQUEST['Android']) ? 'Android' : '')))))));  
+	define('WIRELESS_PROTOCOL', isset($_REQUEST['wap']) ? 'wap' : (isset($_REQUEST['wap2']) ? 'wap2' : (isset($_REQUEST['iphone']) ? 'iphone' : (isset($_REQUEST['imode']) ? 'imode' : (isset($_REQUEST['IEweb']) ? 'IEweb' :(isset($_REQUEST['html']) ? 'html' : (isset($_REQUEST['Android']) ? 'Android' : '')))))));  
 
 if (WIRELESS_PROTOCOL == 'wap')
 	  {
-$browser_t = "mobile";
+$browser_t = "web";
 	  }
 elseif (WIRELESS_PROTOCOL == 'wap2')
 	  {
 
 
-$browser_t = "mobile";
+$browser_t = "web";
 
 
 	  }
 elseif (WIRELESS_PROTOCOL == 'imode')
 	  {
 	
-$browser_t = "mobile";
+$browser_t = "web";
 
 	  }
 	  elseif (WIRELESS_PROTOCOL == 'iphone')
 	  {
 	
 
-$browser_t = "smartphone";
+$browser_t = "web";
 
 	  }
 	  elseif (WIRELESS_PROTOCOL == 'Android')
 	  {
 	
 
-$browser_t = "smartphone";
+$browser_t = "web";
 
 	  }
-	   elseif (WIRELESS_PROTOCOL == 'IEMobile')
+	   elseif (WIRELESS_PROTOCOL == 'IEweb')
 	  {
 	
-$browser_t = "smartphone";
+$browser_t = "web";
 
 	  }
 	  elseif (WIRELESS_PROTOCOL == 'html')
 	  {
 
-	 $mobile_browser = '0';
+	 $web_browser = '0';
 
-if(preg_match('/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone)/i',
+if(preg_match('/(up.browser|up.link|mmp|symbian|web|midp|wap|phone)/i',
     strtolower($_SERVER['HTTP_USER_AGENT']))){
-    $mobile_browser++;
+    $web_browser++;
     }
 
 if((strpos(strtolower($_SERVER['HTTP_ACCEPT']),'application/vnd.wap.xhtml+xml')>0) or 
     ((isset($_SERVER['HTTP_X_WAP_PROFILE']) or isset($_SERVER['HTTP_PROFILE'])))){
-    $mobile_browser++;
+    $web_browser++;
     }
 
-$mobile_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'],0,4));
-$mobile_agents = array(
+$web_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'],0,4));
+$web_agents = array(
     'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
     'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
     'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
@@ -112,27 +112,27 @@ $mobile_agents = array(
     'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
     'wapr','webc','winw','winw','xda','xda-');
 
-if(in_array($mobile_ua,$mobile_agents)){
-    $mobile_browser++;
+if(in_array($web_ua,$web_agents)){
+    $web_browser++;
     }
 if (strpos(strtolower($_SERVER['ALL_HTTP']),'OperaMini')>0) {
-    $mobile_browser++;
+    $web_browser++;
     }
-		if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'iemobile')>0) {
-$mobile_browser++;
+		if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'ieweb')>0) {
+$web_browser++;
 }
 if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'windows')>0) {
-    $mobile_browser=0;
+    $web_browser=0;
     }
 
 
-if($mobile_browser>0){
+if($web_browser>0){
    // do something wap
 
-$browser_t = "mobile";
+$browser_t = "web";
 
 }
-// non-mobile
+// non-web
 else
 {
 
@@ -152,20 +152,20 @@ $browser_t = "web";
 	
 	else
 	{
-$mobile_browser = '0';
+$web_browser = '0';
 
-if(preg_match('/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone)/i',
+if(preg_match('/(up.browser|up.link|mmp|symbian|web|midp|wap|phone)/i',
     strtolower($_SERVER['HTTP_USER_AGENT']))){
-    $mobile_browser++;
+    $web_browser++;
     }
 
 if((strpos(strtolower($_SERVER['HTTP_ACCEPT']),'application/vnd.wap.xhtml+xml')>0) or 
     ((isset($_SERVER['HTTP_X_WAP_PROFILE']) or isset($_SERVER['HTTP_PROFILE'])))){
-    $mobile_browser++;
+    $web_browser++;
     }
 
-$mobile_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'],0,4));
-$mobile_agents = array(
+$web_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'],0,4));
+$web_agents = array(
     'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
     'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
     'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
@@ -176,27 +176,27 @@ $mobile_agents = array(
     'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
     'wapr','webc','winw','winw','xda','xda-');
 
-if(in_array($mobile_ua,$mobile_agents)){
-    $mobile_browser++;
+if(in_array($web_ua,$web_agents)){
+    $web_browser++;
     }
-	if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'iemobile')>0) {
-$mobile_browser++;
+	if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'ieweb')>0) {
+$web_browser++;
 }
 if (strpos(strtolower($_SERVER['ALL_HTTP']),'OperaMini')>0) {
-    $mobile_browser++;
+    $web_browser++;
     }
 if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'windows')>0) {
-    $mobile_browser=0;
+    $web_browser=0;
     }
 
 
-if($mobile_browser>0){
+if($web_browser>0){
    // do something wap
  
-$browser_t = "mobile";
+$browser_t = "web";
 
 }
-// non-mobile
+// non-web
 else
 {
 $_SESSION['Browser_d'] = "web";
